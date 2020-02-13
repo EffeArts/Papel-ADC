05cf8ae98586a152b9a9bf24234c68bf88e596d8
Ishimwe Ayman <ayman.ishimwe@gmail.com>
2020-02-14 01:27:40 +0200

    ch(server-config): Configure Node/Express
    -Installed and configured NodeJs
    - Install and configure ESLINT with Airbnb guidelines
    - Install Express
    
    [Finishes: #171227180]

---
+++ index.js
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+   bodyParser.urlencoded({
+      // eslint-disable-next-line linebreak-style
+      extended: true,
+   }),
+);
+
+app.get('/', (request, response) => {
+   response.json({
+      info: 'Node.js, Express, and Postgres API'
+   });
+});
+
+app.listen(port, () => {
+   console.log(`App running on port ${port}.`);
+});
