93577927cee0e67e5be30cb8525814664654546f
Ishimwe Ayman <ayman.ishimwe@gmail.com>
2020-02-18 01:31:30 +0200

    ft(api-accounts): format JSON response
    - Format JSON response so that it returns with its status code

--- server/data/database.js
+++ server/data/database.js
@@ -1,24 +1,27 @@
-const {
-   Pool,
-} = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-   user: 'admin',
-   host: 'localhost',
-   database: 'papel',
-   password: 'root',
-   port: 5433,
+  user: "admin",
+  host: "localhost",
+  database: "papel",
+  password: "root",
+  port: 5433
 });
 
 const getAccounts = (request, response) => {
-   pool.query('SELECT * FROM accounts ORDER BY id ASC', (error, results) => {
-      if (error) {
-         throw error;
-      }
-      response.status(200).json(results.rows);
-   });
+  pool.query("SELECT * FROM accounts ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    const reply = {
+      status: 200,
+      data: results.rows
+    };
+    response.send(reply);
+    //response.status(200).json(results.rows);
+  });
 };
 
 module.exports = {
-   getAccounts,
+  getAccounts
 };
