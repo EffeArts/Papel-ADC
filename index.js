db1332a8ac33bdc380f322db412fe4aec352cc4e
Ishimwe Ayman <ayman.ishimwe@gmail.com>
2020-02-19 13:07:29 +0200

    feat(API): user signup
    - register a user through an endpoint of POST request '/auth/signup'

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 /* eslint-disable linebreak-style */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const db = require('./server/data/database');
+const db = require("./server/data/database");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(
-   bodyParser.urlencoded({
-      // eslint-disable-next-line linebreak-style
-      extended: true,
-   }),
+  bodyParser.urlencoded({
+    // eslint-disable-next-line linebreak-style
+    extended: true
+  })
 );
 
-app.get('/', (request, response) => {
-   response.json({
-      info: 'Node.js, Express, and Postgres API',
-   });
+app.get("/", (request, response) => {
+  response.json({
+    info: "Node.js, Express, and Postgres API"
+  });
 });
 
-app.get('/accounts', db.getAccounts);
-app.get('/accounts/:acc_num', db.getAccountByNumber);
-app.get('/accounts/:acc_num/transactions', db.getTransactionsByAcc);
+app.get("/accounts", db.getAccounts);
+app.get("/accounts/:acc_num", db.getAccountByNumber);
+app.get("/accounts/:acc_num/transactions", db.getTransactionsByAcc);
 
-app.get('/transactions', db.getTransactions);
-app.get('/transactions/:id', db.getTransactionById);
+app.get("/transactions", db.getTransactions);
+app.get("/transactions/:id", db.getTransactionById);
 
-app.get('/users', db.getUsers);
-app.get('/users/:email/accounts', db.getUserAccounts);
+app.get("/users", db.getUsers);
+app.get("/users/:email/accounts", db.getUserAccounts);
 
+app.post("/auth/signup", db.signup);
 
 app.listen(port, () => {
-   console.log(`App running on port ${port}.`);
+  console.log(`App running on port ${port}.`);
 });
--- server/data/database.js
+++ server/data/database.js
@@ -1,159 +1,182 @@
-const {
-   Pool
-} = require("pg");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-   user: "admin",
-   host: "localhost",
-   database: "papel",
-   password: "root",
-   port: 5433
+  user: "admin",
+  host: "localhost",
+  database: "papel",
+  password: "root",
+  port: 5433
 });
 
 const getUsers = (request, response) => {
-   pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
-      if (error) {
-         throw error;
-      }
-      const reply = {
-         status: 200,
-         data: results.rows
-      };
-      response.send(reply);
-   });
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    const reply = {
+      status: 200,
+      data: results.rows
+    };
+    response.send(reply);
+  });
 };
 
 const getUserAccounts = (request, response) => {
-   const email = request.params.email;
+  const email = request.params.email;
 
-   pool.query(
-      "SELECT accounts.createdOn, accounts.accnumber, accounts.type, accounts.status, accounts.balance  FROM accounts INNER JOIN users ON accounts.owner = users.id  WHERE users.email = $1",
-      [email],
-      (error, results) => {
-         if (error) {
-            throw error;
-         }
-         const reply = {
-            status: 200,
-            data: results.rows
-         };
-         response.send(reply);
+  pool.query(
+    "SELECT accounts.createdOn, accounts.accnumber, accounts.type, accounts.status, accounts.balance  FROM accounts INNER JOIN users ON accounts.owner = users.id  WHERE users.email = $1",
+    [email],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-   );
+      const reply = {
+        status: 200,
+        data: results.rows
+      };
+      response.send(reply);
+    }
+  );
 };
 
 const getAccounts = (request, response) => {
-
-   if (Object.keys(request.query).length === 0) {
-      pool.query("SELECT * FROM accounts ORDER BY id ASC", (error, results) => {
-         if (error) {
-            throw error;
-         }
-         const reply = {
-            status: 200,
-            data: results.rows
-         };
-         response.send(reply);
-      });
-   } else {
-
-      const status = request.query.status;
-      pool.query(
-         "SELECT * FROM accounts WHERE status = $1",
-         [status],
-         (error, results) => {
-            if (error) {
-               throw error;
-            }
-            const reply = {
-               status: 200,
-               data: results.rows
-            };
-            response.send(reply);
-         }
-      );
-
-   }
-
+  if (Object.keys(request.query).length === 0) {
+    pool.query("SELECT * FROM accounts ORDER BY id ASC", (error, results) => {
+      if (error) {
+        throw error;
+      }
+      const reply = {
+        status: 200,
+        data: results.rows
+      };
+      response.send(reply);
+    });
+  } else {
+    const status = request.query.status;
+    pool.query(
+      "SELECT * FROM accounts WHERE status = $1",
+      [status],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        const reply = {
+          status: 200,
+          data: results.rows
+        };
+        response.send(reply);
+      }
+    );
+  }
 };
 
 const getTransactions = (request, response) => {
-   pool.query("SELECT * FROM transactions ORDER BY id ASC", (error, results) => {
-      if (error) {
-         throw error;
-      }
-      const reply = {
-         status: 200,
-         data: results.rows
-      };
-      response.send(reply);
-   });
+  pool.query("SELECT * FROM transactions ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    const reply = {
+      status: 200,
+      data: results.rows
+    };
+    response.send(reply);
+  });
 };
 
 const getAccountByNumber = (request, response) => {
-   const acc_num = parseInt(request.params.acc_num);
+  const acc_num = parseInt(request.params.acc_num);
 
-   pool.query(
-      "SELECT * FROM accounts WHERE accnumber = $1",
-      [acc_num],
-      (error, results) => {
-         if (error) {
-            throw error;
-         }
-         const reply = {
-            status: 200,
-            data: results.rows
-         };
-         response.send(reply);
+  pool.query(
+    "SELECT * FROM accounts WHERE accnumber = $1",
+    [acc_num],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-   );
+      const reply = {
+        status: 200,
+        data: results.rows
+      };
+      response.send(reply);
+    }
+  );
 };
 
-
 const getTransactionsByAcc = (request, response) => {
-   const acc_num = parseInt(request.params.acc_num);
+  const acc_num = parseInt(request.params.acc_num);
 
-   pool.query(
-      "SELECT * FROM transactions WHERE accnumber = $1",
-      [acc_num],
-      (error, results) => {
-         if (error) {
-            throw error;
-         }
-         const reply = {
-            status: 200,
-            data: results.rows
-         };
-         response.send(reply);
+  pool.query(
+    "SELECT * FROM transactions WHERE accnumber = $1",
+    [acc_num],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-   );
+      const reply = {
+        status: 200,
+        data: results.rows
+      };
+      response.send(reply);
+    }
+  );
 };
 
 const getTransactionById = (request, response) => {
-   const id = parseInt(request.params.id);
+  const id = parseInt(request.params.id);
 
-   pool.query(
-      "SELECT * FROM transactions WHERE id = $1",
-      [id],
-      (error, results) => {
-         if (error) {
-            throw error;
-         }
-         const reply = {
-            status: 200,
-            data: results.rows
-         };
-         response.send(reply);
+  pool.query(
+    "SELECT * FROM transactions WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-   );
+      const reply = {
+        status: 200,
+        data: results.rows
+      };
+      response.send(reply);
+    }
+  );
+};
+
+const signup = (request, response) => {
+  const {
+    fname,
+    lname,
+    email,
+    username,
+    password,
+    isadmin,
+    role
+  } = request.body;
+
+  //   response.send(request.body);
+
+  pool.query(
+    "INSERT INTO users (fname, lname, email, username, password, isadmin, role) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [fname, lname, email, username, password, isadmin, role],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      // const reply = {
+      //   status: 201,
+      //   data: results.rows
+      // };
+      response.status(201).send(result.insertId);
+    }
+  );
 };
 
 module.exports = {
-   getAccounts,
-   getTransactionsByAcc,
-   getTransactions,
-   getTransactionById,
-   getAccountByNumber,
-   getUsers,
-   getUserAccounts
+  getAccounts,
+  getTransactionsByAcc,
+  getTransactions,
+  getTransactionById,
+  getAccountByNumber,
+  getUsers,
+  getUserAccounts,
+  signup
 };
